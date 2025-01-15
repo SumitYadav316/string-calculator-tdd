c15be4973f6034251fee85778bda4fa22bb0daca
sumitydavna@gmail.com <sumitydavna@gmail.com>
2025-01-15 16:11:52 +0530

    test for the delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,4 +21,9 @@ RSpec.describe StringCalculator do
     expect(calculator.add("1\n2,3")).to eq(6)
   end
 
+  it "supports different delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
+
 end
