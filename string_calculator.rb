ffe8fc316b02ad8a9785667b063daf4c822280e7
sumitydavna@gmail.com <sumitydavna@gmail.com>
2025-01-15 16:29:45 +0530

    Handle Negative Numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,12 +1,19 @@
 class StringCalculator
+
   def add(numbers)
     return 0 if numbers.empty?
+
     if numbers.start_with?("//")
       delimiter, numbers = numbers[2..].split("\n", 2)
-      numbers.split(delimiter).map(&:to_i).sum
+      numbers = numbers.split(delimiter)
     else
-      numbers.split(/,|\n/).map(&:to_i).sum
+      numbers = numbers.split(/,|\n/)
     end
-  end
-end
 
+    negatives = numbers.map(&:to_i).select(&:negative?)
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    numbers.map(&:to_i).sum
+  end
+
+end
