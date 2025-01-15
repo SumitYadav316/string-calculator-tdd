896e54be2f042c2bd802d7a595ef86afba92d893
sumitydavna@gmail.com <sumitydavna@gmail.com>
2025-01-15 15:58:35 +0530

    test for newline

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe StringCalculator do
     expect(calculator.add("1,2")).to eq(3)
   end
 
+  it "handles newlines between numbers" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1\n2,3")).to eq(6)
+  end
+
 end
